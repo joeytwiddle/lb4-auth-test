d029c2d2cc8cc5d87875fa5b1931c6d535521e79
Paul Joey Clark <joeytwiddle@gmail.com>
2018-05-03 18:18:22 +0800

    Added a test to check that auth is working

---
+++ test/user.controller.acceptance.ts
@@ -0,0 +1,52 @@
+import {createClientForHandler, supertest} from '@loopback/testlab';
+import {RestServer} from '@loopback/rest';
+import {AuthApplication} from '../';
+
+describe('UserController', () => {
+  let app: AuthApplication;
+  let server: RestServer;
+  let client: supertest.SuperTest<supertest.Test>;
+
+  before(givenAnApplication);
+
+  before(givenARestServer);
+
+  before(async () => {
+    await app.boot();
+    await app.start();
+  });
+
+  before(() => {
+    client = createClientForHandler(server.requestHandler);
+  });
+
+  after(async () => {
+    await app.stop();
+  });
+
+  it('invokes GET /whoami expecting failure', async () => {
+    await client
+      .get('/whoami')
+      .auth('BUST', 'BUST')
+      .expect(401);
+  });
+
+  it('invokes GET /whoami expecting failure', async () => {
+    await client
+      .get('/whoami')
+      .auth('test', 'test')
+      .expect(200);
+  });
+
+  function givenAnApplication() {
+    app = new AuthApplication({
+      rest: {
+        port: 0,
+      },
+    });
+  }
+
+  async function givenARestServer() {
+    server = await app.getServer(RestServer);
+  }
+});
